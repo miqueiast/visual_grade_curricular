8db46f37bdc6c8c41b9439438df5e8751548c1b0
miqueiast <miqueias.teixeira@gmail.com>
2023-12-01 14:43:50 -0300

    Merge remote-tracking branch 'origin/main'

--- estatistica_UFPR.py
+++ estatistica_UFPR.py
@@ -3,24 +3,70 @@ import pandas as pd
 import io
 import requests
 
+
+# Função para carregar os dados
+def carregar_dados(url):
+    response = requests.get(url)
+    return pd.read_excel(io.BytesIO(response.content), engine='openpyxl')
+
+
+# Função para mapear código para nome
+def mapear_codigo_para_nome(df):
+    return dict(zip(df['Código'], df['Nome']))
+
+
+# Função para adicionar informações de pré-requisitos
+def adicionar_info_pre_requisitos(row, codigo_para_nome, codigo_para_periodo, cor_por_tipo):
+    tipo_disciplina = row['Código'][:2]
+    cor = cor_por_tipo.get(tipo_disciplina, 'DarkGrey')
+
+    discipline_expander = st.expander(f'{row["Código"]} - {row["Nome"]}', expanded=False)
+
+    with discipline_expander:
+        st.write(f"Carga Horária: {row['Carga Horária']}")
+        pre_requisitos_msg = obter_mensagem_requisitos(row['Pré-requisito'], codigo_para_nome, codigo_para_periodo)
+        st.write(pre_requisitos_msg)
+        requisitos_msg = obter_mensagem_requisitos(row['Requisito'], codigo_para_nome, codigo_para_periodo)
+        st.write(requisitos_msg)
+        st.write(f"Ementa: {row['Ementa']}")
+        st.write(f"Bibliografia: {row['Bibliografia']}")
+
+
+# Função para obter mensagem de pré-requisitos ou requisitos
+def obter_mensagem_requisitos(requisitos, codigo_para_nome, codigo_para_periodo):
+    requisitos_nomes = obter_nomes_e_periodos(requisitos, codigo_para_nome, codigo_para_periodo)
+    return "Requisito: " + "\n".join(requisitos_nomes) if requisitos_nomes else "Requisito: Nenhum"
+
+
+# Função para obter nomes e períodos a partir de códigos
+def obter_nomes_e_periodos(codigos, codigo_para_nome, codigo_para_periodo):
+    if pd.notna(codigos):
+        codigos = str(codigos)  # Certificar-se de que é uma string
+        if '/' in codigos:
+            return [
+                f"{codigo} - {codigo_para_nome.get(codigo, '')} ({codigo_para_periodo.get(codigo, '')})"
+                for codigo in codigos.split('/') if codigo
+            ]
+        else:
+            return [f"{codigos} - {codigo_para_nome.get(codigos, '')} ({codigo_para_periodo.get(codigos, '')})"]
+    else:
+        return []
+
+
 # Carregamento dos dados do link
 url = 'https://github.com/miqueiast/visual_grade_curricular/raw/main/grade_curricular.xlsx'
-response = requests.get(url)
-df = pd.read_excel(io.BytesIO(response.content), engine='openpyxl')
+df = carregar_dados(url)
 
 # Mapear Código para Nome para facilitar a busca de pré-requisitos
-codigo_para_nome = dict(zip(df['Código'], df['Nome']))
+codigo_para_nome = mapear_codigo_para_nome(df)
 codigo_para_periodo = dict(zip(df['Código'], df['Período']))
 
 # Adicionar coluna 'Requisito' ao DataFrame
 df['Requisito'] = ''
 
 # Preencher a coluna 'Requisito' com base nas informações de pré-requisito
-for index, row in df.iterrows():
-    disciplina_codigo = row['Código']
-    requisitos_para_disciplina = df[df['Pré-requisito'].str.contains(disciplina_codigo, na=False)]
-    requisitos_codigos = requisitos_para_disciplina['Código'].tolist()
-    df.at[index, 'Requisito'] = '/'.join(requisitos_codigos)
+df['Requisito'] = df['Código'].apply(
+    lambda x: '/'.join(df[df['Pré-requisito'].str.contains(x, na=False)]['Código'].tolist()))
 
 # Agrupar por Período
 periodos = df['Período'].unique()
@@ -28,8 +74,20 @@ periodos = df['Período'].unique()
 # Configurar o layout da página
 st.set_page_config(layout="wide")
 
+# Criar duas colunas para posicionar as imagens lado a lado
+col1, col2 = st.columns(2)
+
+# Adicionar a primeira imagem na primeira coluna
+col1.image("estatisticacienciadedados.png", width=720)  # Substitua pelo caminho da sua primeira imagem e ajuste a largura conforme necessário
+
+# Adicionar espaçamento entre as colunas usando st.markdown
+col1.markdown("&nbsp;", unsafe_allow_html=True)
+
+# Adicionar a segunda imagem na segunda coluna
+col2.image("personalgithubpage.png", width=200)  # Substitua pelo caminho da sua segunda imagem e ajuste a largura conforme necessário
+
 # Adicionar um título
-st.title("Visualização de Grade Curricular")
+st.title("Visualização da Grade Curricular")
 
 # Criar um contêiner para os períodos
 container = st.container()
@@ -52,7 +110,8 @@ script = """
 st.markdown(script, unsafe_allow_html=True)
 
 # Defina uma paleta de cores para os períodos
-cores_por_periodo = {'1': 'LightBlue', '2': 'LightGreen', '3': 'LightSalmon', '4': 'LightPink', '5': 'LightGoldenRodYellow'}
+cores_por_periodo = {'1': 'LightBlue', '2': 'LightGreen', '3': 'LightSalmon', '4': 'LightPink',
+                     '5': 'LightGoldenRodYellow'}
 
 # Criar uma seção para cada Período
 for periodo in sorted(periodos):
@@ -68,62 +127,12 @@ for periodo in sorted(periodos):
                 f'<button class="period-button" style="height: {altura_botao}px; width: 100px; margin-top: 5px; background-color: {cor_background};">{periodo}</button>',
                 unsafe_allow_html=True
             )
-        
+
         # Criar uma coluna para as disciplinas
         with col2:
             # Filtrar o DataFrame para o período específico
             periodo_df = df[df['Período'] == periodo]
-            
+
             # Exibir as disciplinas do período
             for i, (index, row) in enumerate(periodo_df.iterrows()):
-                tipo_disciplina = row['Código'][:2]
-                cor = cor_por_tipo.get(tipo_disciplina, 'DarkGrey')
-
-                # Definir um espaçamento adicional para a primeira disciplina
-                margin_top = '50px' if i == 0 else '5px'
-
-                # Estilizar a disciplina com base na cor
-                discipline_expander = st.expander(f'{row["Código"]} - {row["Nome"]}', expanded=False)
-                
-                with discipline_expander:
-                    st.write(f"Carga Horária: {row['Carga Horária']}")
-                    
-                    # Adicionar informações de pré-requisitos
-                    pre_requisitos = row['Pré-requisito']
-                    if pd.notna(pre_requisitos):
-                        pre_requisitos_nomes = [
-                            f"{codigo} - {codigo_para_nome.get(codigo, '')} ({codigo_para_periodo.get(codigo, '')})"
-                            for codigo in pre_requisitos.split('/')
-                        ]
-                        pre_requisitos_nomes = [nome for nome in pre_requisitos_nomes if nome]  # Remover valores nulos
-                        if pre_requisitos_nomes:
-                            st.write("Pré-requisito:")
-                            for nome in pre_requisitos_nomes:
-                                st.write(nome)
-                        else:
-                            st.write("Pré-requisito: Nenhum")
-                    else:
-                        st.write("Pré-requisito: Nenhum")
-
-                    # Adicionar informações de pré-requisitos
-                    requisitos = row['Requisito']
-                    if pd.notna(requisitos) and requisitos != "":
-                        requisitos_nomes = [
-                            f"{codigo} - {codigo_para_nome.get(codigo, '')} ({codigo_para_periodo.get(codigo, '')})"
-                            for codigo in requisitos.split('/')
-                        ]
-                        requisitos_nomes = [nome for nome in requisitos_nomes if nome]  # Remove null values
-                        if requisitos_nomes:
-                            st.write("Requisito:")
-                            for nome in requisitos_nomes:
-                                st.write(nome)
-                        else:
-                            st.write("Requisito: Nenhum")
-                    else:
-                        st.write("Requisito: Nenhum")
-
-
-
-                                
-                    st.write(f"Ementa: {row['Ementa']}")
-                    st.write(f"Bibliografia: {row['Bibliografia']}")
+                adicionar_info_pre_requisitos(row, codigo_para_nome, codigo_para_periodo, cor_por_tipo)
